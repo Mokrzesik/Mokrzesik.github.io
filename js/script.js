83be60220bdca002d185f11ee7640137d5dea1b9
Mokrzesik <Mjo14@illinois.edu>
2024-07-12 11:35:12 -0500

    updated script.js
    made the visualization inside the loading data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let data;
-
 async function loadCSV() {
     try {
       const response = await fetch('https://flunky.github.io/cars2017.csv');
@@ -14,39 +12,42 @@ async function loadCSV() {
         skipEmptyLines: true
       });
   
-      data = parsedData.data; // This is your constant with the loaded data
-      console.log(data); // You can use or manipulate the data as needed
+      const data = parsedData.data; // This is your constant with the loaded data
+
+      createVisualization(data); // 
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
     }
   }
+
+function createVisualization(data) {
+    const svg = d3.select("svg").append("g").attr("transform", "translate(50,50)");
+
+    const xScale = d3.scaleLog().domain([10,150]).range([0,200]);
+    const yScale = d3.scaleLog().domain([10,150]).range([200,0]);
+
+    svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) {
+        return (xScale(parseInt(d.AverageCityMPG)))
+        })
+        .attr("cy", function(d) {
+        return (yScale(parseInt(d.AverageHighwayMPG)))
+        })
+        .attr("r", function(d) {
+        return (2 + parseInt(d.EngineCylinders))
+        });
+
+    var xAxis = d3.axisBottom(xScale).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
+    var yAxis = d3.axisLeft(yScale).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
+
+    d3.select("svg").append("g").attr("transform", "translate(50,50)").call(yAxis);
+    d3.select("svg").append("g").attr("transform", "translate(50,250)").call(xAxis);
+}
   
 // Call the function to load the data
 loadCSV();
-  
-const svg = d3.select("svg").append("g").attr("transform", "translate(50,50)");
 
-const xScale = d3.scaleLog().domain([10,150]).range([0,200]);
-const yScale = d3.scaleLog().domain([10,150]).range([200,0]);
-
-svg.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d) {
-    return (xScale(parseInt(d.AverageCityMPG)))
-    })
-    .attr("cy", function(d) {
-    return (yScale(parseInt(d.AverageHighwayMPG)))
-    })
-    .attr("r", function(d) {
-    return (2 + parseInt(d.EngineCylinders))
-    });
-
-var xAxis = d3.axisBottom(xScale).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
-var yAxis = d3.axisLeft(yScale).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
-
-d3.select("svg").append("g").attr("transform", "translate(50,50)").call(yAxis);
-d3.select("svg").append("g").attr("transform", "translate(50,250)").call(xAxis);
-
-//test0.0.7
+//test0.0.9

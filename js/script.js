5756633ffcc303d442188104c8c52984e72f5ff0
Mokrzesik <Mjo14@illinois.edu>
2024-07-12 10:04:59 -0500

    changed it to the last thing we did in class

--- js/script.js
+++ js/script.js
@@ -1,35 +1,25 @@
-// Define dataset (can be dynamic data from an API or static)
-const dataset = [10, 20, 30, 40, 50];
+const data = await d3.csv("https://flunky.github.io/cars2017.csv");
+const svg = d3.select("svg").append("g").attr("transform", "translate(50,50)");
 
-// Set up dimensions for the chart
-const width = 500;
-const height = 300;
-const barPadding = 5;
+const xScale = d3.scaleLog().domain([10,150]).range([0,200]);
+const yScale = d3.scaleLog().domain([10,150]).range([200,0]);
 
-// Create SVG element
-const svg = d3.select("#chart-container")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-// Create bars
-svg.selectAll("rect")
-    .data(dataset)
+svg.selectAll("circle")
+    .data(data)
     .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * (width / dataset.length))
-    .attr("y", d => height - d * 5) // Scale the height of bars
-    .attr("width", width / dataset.length - barPadding)
-    .attr("height", d => d * 5) // Scale the height of bars
-    .attr("fill", "teal");
+    .append("circle")
+    .attr("cx", function(d) {
+    return (xScale(parseInt(d.AverageCityMPG)))
+    })
+    .attr("cy", function(d) {
+    return (yScale(parseInt(d.AverageHighwayMPG)))
+    })
+    .attr("r", function(d) {
+    return (2 + parseInt(d.EngineCylinders))
+    });
 
-// Optional: Add labels to bars
-svg.selectAll("text")
-    .data(dataset)
-    .enter()
-    .append("text")
-    .text(d => d)
-    .attr("x", (d, i) => i * (width / dataset.length) + (width / dataset.length - barPadding) / 2)
-    .attr("y", d => height - d * 5 + 15) // Adjust position for better alignment
-    .attr("text-anchor", "middle")
-    .attr("fill", "white");
+var xAxis = d3.axisBottom(xScale).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
+var yAxis = d3.axisLeft(yScale).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
+
+d3.select("svg").append("g").attr("transform", "translate(50,50)").call(yAxis);
+d3.select("svg").append("g").attr("transform", "translate(50,250)").call(xAxis);

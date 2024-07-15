2c0a9013df7c4d8db69e8b2d4b3a80de296c44aa
Mokrzesik <Mjo14@illinois.edu>
2024-07-15 17:10:06 -0500

    lot of progress

--- js/script.js
+++ js/script.js
@@ -4,15 +4,19 @@ async function loadCSV() {
       if (!response.ok) {
         throw new Error('Network response was not ok ' + response.statusText);
       }
-      const csvText = await response.text();
+      let csvText = await response.text();
+      // Remove null characters
+      csvText = csvText.replace(/\u0000/g, '');
       
       // Now, you can parse the CSV data as needed
       const parsedData = Papa.parse(csvText, {
         header: true, // Use headers as keys
-        skipEmptyLines: true
+        skipEmptyLines: true,
+        transformHeader: header => header.trim().toLowerCase()
       });
   
       const data = parsedData.data; // This is your constant with the loaded data
+
       const gen1 = data.filter(item => item.gen === 'I');
       const gen2 = data.filter(item => item.gen === 'II');
       const gen3 = data.filter(item => item.gen === 'III');
@@ -22,44 +26,15 @@ async function loadCSV() {
       const gen7 = data.filter(item => item.gen === 'VII');
       const gen8 = data.filter(item => item.gen === 'VIII');
 
-      let gen1_attack = 0;
-      let gen1_hp = 0;
-      let gen1_defense = 0;
-      let gen1_sp_attack = 0;
-      let gen1_sp_defense = 0;
-      let gen1_speed = 0;
-      const total_gen1_pokemon = gen1.length;
-      console.log(total_gen1_pokemon);
+      let [gen1_attackAvg, gen1_hpAvg, gen1_defenseAvg, gen1_sp_attackAvg, gen1_sp_defenseAvg, gen1_speedAvg] = extractGenData(gen1);
+      let [gen2_attackAvg, gen2_hpAvg, gen2_defenseAvg, gen2_sp_attackAvg, gen2_sp_defenseAvg, gen2_speedAvg] = extractGenData(gen2);
+      let [gen3_attackAvg, gen3_hpAvg, gen3_defenseAvg, gen3_sp_attackAvg, gen3_sp_defenseAvg, gen3_speedAvg] = extractGenData(gen3);
+      let [gen4_attackAvg, gen4_hpAvg, gen4_defenseAvg, gen4_sp_attackAvg, gen4_sp_defenseAvg, gen4_speedAvg] = extractGenData(gen4);
+      let [gen5_attackAvg, gen5_hpAvg, gen5_defenseAvg, gen5_sp_attackAvg, gen5_sp_defenseAvg, gen5_speedAvg] = extractGenData(gen5);
+      let [gen6_attackAvg, gen6_hpAvg, gen6_defenseAvg, gen6_sp_attackAvg, gen6_sp_defenseAvg, gen6_speedAvg] = extractGenData(gen6);
+      let [gen7_attackAvg, gen7_hpAvg, gen7_defenseAvg, gen7_sp_attackAvg, gen7_sp_defenseAvg, gen7_speedAvg] = extractGenData(gen7);
+      let [gen8_attackAvg, gen8_hpAvg, gen8_defenseAvg, gen8_sp_attackAvg, gen8_sp_defenseAvg, gen8_speedAvg] = extractGenData(gen8);
 
-      for (let i = 0; i < total_gen1_pokemon; i++) {
-        gen1_attack += gen1[i].attack;
-        console.log(gen1[i].attack);
-        gen1_hp += gen1[i].hp;
-        gen1_defense += gen1[i].defense;
-        gen1_sp_attack += gen1[i].sp_attack;
-        gen1_sp_defense += gen1[i].sp_defesnse;
-        gen1_sp_speed += gen1[i].speed;
-      }
-      console.log(gen1_attack);
-      console.log(gen1_hp);
-      console.log(gen1_defense);
-      console.log(gen1_sp_attack);
-      console.log(gen1_sp_defense);
-      console.log(gen1_speed);
-
-      gen1_attack = gen1_attack / total_gen1_pokemon;
-      gen1_hp = gen1_hp / total_gen1_pokemon;
-      gen1_defense = gen1_defense / total_gen1_pokemon;
-      gen1_sp_attack = gen1_sp_attack / total_gen1_pokemon;
-      gen1_sp_defense = gen1_sp_defense / total_gen1_pokemon;
-      gen1_speed = gen1_speed / total_gen1_pokemon;
-
-      console.log(gen1_attack);
-      console.log(gen1_hp);
-      console.log(gen1_defense);
-      console.log(gen1_sp_attack);
-      console.log(gen1_sp_defense);
-      console.log(gen1_speed);
 
       createVisualization(gen1); // Call the graph making
     } catch (error) {
@@ -69,31 +44,41 @@ async function loadCSV() {
 
 function createVisualization(data) {
 
-  const svg = d3.select("svg").append("g").attr("transform", "translate(50,50)");
+}
 
-  const xScale = d3.scaleLog().domain([10,150]).range([0,300]);
-  const yScale = d3.scaleLog().domain([10,150]).range([300,0]);
+function extractGenData(gen) {
+  let gen_attack = 0;
+  let gen_hp = 0;
+  let gen_defense = 0;
+  let gen_sp_attack = 0;
+  let gen_sp_defense = 0;
+  let gen_speed = 0;
+  const total_gen_pokemon = gen.length;
 
-  svg.selectAll("circle")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("cx", function(d) {
-      return (xScale(parseInt(d.national_number)))
-      })
-      .attr("cy", function(d) {
-      return (yScale(parseInt(d.attack)))
-      })
-      .attr("r", function(d) {
-      return (2 + d.weight_kg)
-      });
+  for (let i = 0; i < total_gen_pokemon; i++) {
+    gen_attack += parseInt(gen[i].attack);
+    gen_hp += parseInt(gen[i].hp);
+    gen_defense += parseInt(gen[i].defense);
+    gen_sp_attack += parseInt(gen[i].sp_attack);
+    gen_sp_defense += parseInt(gen[i].sp_defense);
+    gen_speed += parseInt(gen[i].speed);
+  }
 
-  var xAxis = d3.axisBottom(xScale).tickValues([0,,50,100,150,200,250,300]).tickFormat(d3.format("~s"));
-  var yAxis = d3.axisLeft(yScale).tickValues([0,50,100,150,200,250,300]).tickFormat(d3.format("~s"));
+  gen_attack = gen_attack / total_gen_pokemon;
+  gen_hp = gen_hp / total_gen_pokemon;
+  gen_defense = gen_defense / total_gen_pokemon;
+  gen_sp_attack = gen_sp_attack / total_gen_pokemon;
+  gen_sp_defense = gen_sp_defense / total_gen_pokemon;
+  gen_speed = gen_speed / total_gen_pokemon;
 
-  d3.select("svg").append("g").attr("transform", "translate(50,50)").call(yAxis);
-  d3.select("svg").append("g").attr("transform", "translate(50,250)").call(xAxis);
-  console.log("Got to the end of the visualization function")
+  gen_attackAvg = parseInt(gen_attack);
+  gen_hpAvg = parseInt(gen_hp);
+  gen_defenseAvg = parseInt(gen_defense);
+  gen_sp_attackAvg = parseInt(gen_sp_attack);
+  gen_sp_defenseAvg = parseInt(gen_sp_defense);
+  gen_speedAvg = parseInt(gen_speed);
+
+  return [gen_attackAvg, gen_hpAvg, gen_defenseAvg, gen_sp_attackAvg, gen_sp_defenseAvg, gen_speedAvg];
 }
   
 // Call the function to load the data and create visualization

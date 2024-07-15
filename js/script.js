ae0da0b48d057be5f761c72fc9add48e3dcd33ca
Mokrzesik <Mjo14@illinois.edu>
2024-07-15 17:51:16 -0500

    stuff

--- js/script.js
+++ js/script.js
@@ -26,23 +26,126 @@ async function loadCSV() {
       const gen7 = data.filter(item => item.gen === 'VII');
       const gen8 = data.filter(item => item.gen === 'VIII');
 
-      let [gen1_attackAvg, gen1_hpAvg, gen1_defenseAvg, gen1_sp_attackAvg, gen1_sp_defenseAvg, gen1_speedAvg] = extractGenData(gen1);
-      let [gen2_attackAvg, gen2_hpAvg, gen2_defenseAvg, gen2_sp_attackAvg, gen2_sp_defenseAvg, gen2_speedAvg] = extractGenData(gen2);
-      let [gen3_attackAvg, gen3_hpAvg, gen3_defenseAvg, gen3_sp_attackAvg, gen3_sp_defenseAvg, gen3_speedAvg] = extractGenData(gen3);
-      let [gen4_attackAvg, gen4_hpAvg, gen4_defenseAvg, gen4_sp_attackAvg, gen4_sp_defenseAvg, gen4_speedAvg] = extractGenData(gen4);
-      let [gen5_attackAvg, gen5_hpAvg, gen5_defenseAvg, gen5_sp_attackAvg, gen5_sp_defenseAvg, gen5_speedAvg] = extractGenData(gen5);
-      let [gen6_attackAvg, gen6_hpAvg, gen6_defenseAvg, gen6_sp_attackAvg, gen6_sp_defenseAvg, gen6_speedAvg] = extractGenData(gen6);
-      let [gen7_attackAvg, gen7_hpAvg, gen7_defenseAvg, gen7_sp_attackAvg, gen7_sp_defenseAvg, gen7_speedAvg] = extractGenData(gen7);
-      let [gen8_attackAvg, gen8_hpAvg, gen8_defenseAvg, gen8_sp_attackAvg, gen8_sp_defenseAvg, gen8_speedAvg] = extractGenData(gen8);
+      let [gen1_attackAvg, gen1_hpAvg, gen1_defenseAvg, gen1_sp_attackAvg, gen1_sp_defenseAvg, gen1_speedAvg, gen1_avg] = extractGenData(gen1);
+      let [gen2_attackAvg, gen2_hpAvg, gen2_defenseAvg, gen2_sp_attackAvg, gen2_sp_defenseAvg, gen2_speedAvg, gen2_avg] = extractGenData(gen2);
+      let [gen3_attackAvg, gen3_hpAvg, gen3_defenseAvg, gen3_sp_attackAvg, gen3_sp_defenseAvg, gen3_speedAvg, gen3_avg] = extractGenData(gen3);
+      let [gen4_attackAvg, gen4_hpAvg, gen4_defenseAvg, gen4_sp_attackAvg, gen4_sp_defenseAvg, gen4_speedAvg, gen4_avg] = extractGenData(gen4);
+      let [gen5_attackAvg, gen5_hpAvg, gen5_defenseAvg, gen5_sp_attackAvg, gen5_sp_defenseAvg, gen5_speedAvg, gen5_avg] = extractGenData(gen5);
+      let [gen6_attackAvg, gen6_hpAvg, gen6_defenseAvg, gen6_sp_attackAvg, gen6_sp_defenseAvg, gen6_speedAvg, gen6_avg] = extractGenData(gen6);
+      let [gen7_attackAvg, gen7_hpAvg, gen7_defenseAvg, gen7_sp_attackAvg, gen7_sp_defenseAvg, gen7_speedAvg, gen7_avg] = extractGenData(gen7);
+      let [gen8_attackAvg, gen8_hpAvg, gen8_defenseAvg, gen8_sp_attackAvg, gen8_sp_defenseAvg, gen8_speedAvg, gen8_avg] = extractGenData(gen8);
+/*
+      console.log(gen1_attackAvg);
+      console.log(gen2_attackAvg);
+      console.log(gen3_attackAvg);
+      console.log(gen4_attackAvg);
+      console.log(gen5_attackAvg);
+      console.log(gen6_attackAvg);
+      console.log(gen7_attackAvg);
+      console.log(gen8_attackAvg);
 
+      console.log(" ");
 
-      createVisualization(gen1); // Call the graph making
+      console.log(gen1_hpAvg);
+      console.log(gen2_hpAvg);
+      console.log(gen3_hpAvg);
+      console.log(gen4_hpAvg);
+      console.log(gen5_hpAvg);
+      console.log(gen6_hpAvg);
+      console.log(gen7_hpAvg);
+      console.log(gen8_hpAvg);
+
+      console.log(" ");
+
+      console.log(gen1_defenseAvg);
+      console.log(gen2_defenseAvg);
+      console.log(gen3_defenseAvg);
+      console.log(gen4_defenseAvg);
+      console.log(gen5_defenseAvg);
+      console.log(gen6_defenseAvg);
+      console.log(gen7_defenseAvg);
+      console.log(gen8_defenseAvg);
+
+      console.log(" ");
+
+      console.log(gen1_sp_attackAvg);
+      console.log(gen2_sp_attackAvg);
+      console.log(gen3_sp_attackAvg);
+      console.log(gen4_sp_attackAvg);
+      console.log(gen5_sp_attackAvg);
+      console.log(gen6_sp_attackAvg);
+      console.log(gen7_sp_attackAvg);
+      console.log(gen8_sp_attackAvg);
+
+      console.log(" ");
+
+      console.log(gen1_sp_defenseAvg);
+      console.log(gen2_sp_defenseAvg);
+      console.log(gen3_sp_defenseAvg);
+      console.log(gen4_sp_defenseAvg);
+      console.log(gen5_sp_defenseAvg);
+      console.log(gen6_sp_defenseAvg);
+      console.log(gen7_sp_defenseAvg);
+      console.log(gen8_sp_defenseAvg);
+
+      console.log(" ");
+
+      console.log(gen1_speedAvg);
+      console.log(gen2_speedAvg);
+      console.log(gen3_speedAvg);
+      console.log(gen4_speedAvg);
+      console.log(gen5_speedAvg);
+      console.log(gen6_speedAvg);
+      console.log(gen7_speedAvg);
+      console.log(gen8_speedAvg);
+
+      console.log(" ");
+
+      console.log(gen1_avg);
+      console.log(gen2_avg);
+      console.log(gen3_avg);
+      console.log(gen4_avg);
+      console.log(gen5_avg);
+      console.log(gen6_avg);
+      console.log(gen7_avg);
+      console.log(gen8_avg);
+*/
+
+      createVisualization([gen1_attackAvg, gen1_hpAvg, gen1_defenseAvg, gen1_sp_attackAvg, gen1_sp_defenseAvg, gen1_speedAvg, gen1_avg]); // Call the graph making
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
     }
   }
 
-function createVisualization(data) {
+function createVisualization(gen) {
+
+  const attributes = ["attack", "hp", "defense", "sp_attack", "sp_defense", "speed"];
+  const width = 400;
+  const height = 400;
+
+  const averages = attributes.map((attribute, index) => ({
+    attr: attribute,
+    value: gen[index]
+}));
+
+  const svg = d3.select('#Gen1');
+
+  const xScale = d3.scaleBand()
+    .domain(attributes)
+    .range([0,width]);
+
+  const yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([height, 0]);
+
+  svg.selectAll('rect')
+    .data(averages)
+    .enter()
+    .append('rect')
+    .attr('x', d => xScale(d.category))
+    .attr('width', xScale.bandwidth())
+    .attr('y', d => yScale(d.value))
+    .attr('height', d => height - yScale(d.value));
 
 }
 
@@ -78,7 +181,9 @@ function extractGenData(gen) {
   gen_sp_defenseAvg = parseInt(gen_sp_defense);
   gen_speedAvg = parseInt(gen_speed);
 
-  return [gen_attackAvg, gen_hpAvg, gen_defenseAvg, gen_sp_attackAvg, gen_sp_defenseAvg, gen_speedAvg];
+  gen_avg = gen_attackAvg + gen_hpAvg + gen_defenseAvg + gen_sp_attackAvg + gen_sp_defenseAvg + gen_speedAvg;
+
+  return [gen_attackAvg, gen_hpAvg, gen_defenseAvg, gen_sp_attackAvg, gen_sp_defenseAvg, gen_speedAvg, gen_avg];
 }
   
 // Call the function to load the data and create visualization

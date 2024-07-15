c863216e5e0a388f2afff8ec18ee4b463f9ba6e9
Mokrzesik <Mjo14@illinois.edu>
2024-07-15 10:14:16 -0500

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 async function loadCSV() {
     try {
-      const response = await fetch('https://www.kaggle.com/datasets/lashagoch/life-expectancy-who-updated/download');
+      const response = await fetch('https://gist.githubusercontent.com/aishwarya8615/89d9f36fc014dea62487f7347864d16a/raw/8629d284e13976dcb13bb0b27043224b9266fffa/Life_Expectancy_Data.csv');
       if (!response.ok) {
         throw new Error('Network response was not ok ' + response.statusText);
       }
@@ -21,31 +21,31 @@ async function loadCSV() {
   }
 
 function createVisualization(data) {
-    const svg = d3.select("svg").append("g").attr("transform", "translate(50,50)");
+  const svg = d3.select("svg").append("g").attr("transform", "translate(50,50)");
 
-    const xScale = d3.scaleLog().domain([10,150]).range([0,200]);
-    const yScale = d3.scaleLog().domain([10,150]).range([200,0]);
+  const xScale = d3.scaleLog().domain([10,150]).range([0,200]);
+  const yScale = d3.scaleLog().domain([10,150]).range([200,0]);
 
-    svg.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", function(d) {
-        return (xScale(parseInt(d.Hepatitis_B)))
-        })
-        .attr("cy", function(d) {
-        return (yScale(parseInt(d.Infant_deaths)))
-        })
-        .attr("r", function(d) {
-        return (2 + 2)
-        });
+  svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", function(d) {
+      return (xScale(parseInt(d.Hepatitis_B)))
+      })
+      .attr("cy", function(d) {
+      return (yScale(parseInt(d.Infant_deaths)))
+      })
+      .attr("r", function(d) {
+      return (2 + 2)
+      });
 
-    var xAxis = d3.axisBottom(xScale).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
-    var yAxis = d3.axisLeft(yScale).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
+  var xAxis = d3.axisBottom(xScale).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
+  var yAxis = d3.axisLeft(yScale).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
 
-    d3.select("svg").append("g").attr("transform", "translate(50,50)").call(yAxis);
-    d3.select("svg").append("g").attr("transform", "translate(50,250)").call(xAxis);
-    console.log("Got to the end of the visualization function")
+  d3.select("svg").append("g").attr("transform", "translate(50,50)").call(yAxis);
+  d3.select("svg").append("g").attr("transform", "translate(50,250)").call(xAxis);
+  console.log("Got to the end of the visualization function")
 }
   
 // Call the function to load the data and create visualization
